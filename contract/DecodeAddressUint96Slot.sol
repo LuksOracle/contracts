c53e3db326e6f75d43abf10bd2692563969faaf0
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-08-25 14:57:46 -0400

    Update DecodeAddressUint96Slot.sol

--- contract/DecodeAddressUint96Slot.sol
+++ contract/DecodeAddressUint96Slot.sol
@@ -4,14 +4,18 @@ pragma solidity 0.8.16;
 contract DecodeAddressUint96Slot { 
 
     //Values to encode
-    uint public tempRequestAddress = 1102552892404058133515520251406936644975802420559;
-    uint public tempTwitter_id = 1438606749389541377;          //12 BYTES, 32/32 FOR SLOT 0
+    //Uint address: 1102552892404058133515520251406936644975802420559
+    uint public tempRequestAddress = uint(uint160(0xc1202e7d42655F23097476f6D48006fE56d38d4f));
+    //Shifted: 2102526119708102644685250484424962510481439752012496174033982717952
+    uint public tempTwitter_id = 1438606749389541377;          
+    uint public shiftedTempTwitter_id = tempTwitter_id<<160;          
+
 
     //mix = 2102526119708102644685250484424962510481439752012496174033982717952
-    uint public mix = (tempTwitter_id<<160) + tempRequestAddress;
+    uint public mix = shiftedTempTwitter_id + tempRequestAddress;
 
     //Decode
     uint public decodeTwitterID = (mix>>160);
-    uint public decodeAddressID = mix & 0x000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+    address public decodeAddressID = address(uint160(mix & 0x000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF));
 
 }
